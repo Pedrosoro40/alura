4969256a5eab204289be95e32eb29651070832f6
Ronaldo Ribeiro Pedroso <ronaldopedroso40@gmail.com>
2025-11-21 10:42:43 -0300

    Update script.js

--- Js/script.js
+++ Js/script.js
@@ -1,7 +1,20 @@
+let cardContainer = document.querySelector('.card-container');
 let dados = [];
 
 async function iniciarBusca() {
     let resposta = await fetch('Js/data.json');
     dados = await resposta.json();
-    console.log(dados);
+    renderizarCards(dados);
+}
+
+function renderizarCards(dados) {
+    for (let dado of dados) {
+        let article = document.createElement('article');
+        article.classList.add('card');
+        article.innerHTML = `<h2>${dado.name}</h2>
+                <p>${dado.ano}</p>
+                <p>${dado.descricao}.</p>
+                <a href="${dado.link}"_blank">Saiba mais</a>`
+        cardContainer.appendChild(article);
+    }
 }
